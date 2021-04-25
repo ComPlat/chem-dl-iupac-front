bdedcfb4d13ccece6e2f555e8b57649181a97dca
maipy <ira.maier@kit.edu>
2021-04-25 21:36:11 +0200

    add frontend w/o css

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'uglifier', '>= 1.3.0'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
+
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
@@ -21,8 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-#gem 'ketcherails', '~> 0.1.7', git: 'https://github.com/ComPlat/ketcher-rails/tree/rails_5_2'
-gem 'ketcherails', git: 'https://github.com/ComPlat/ketcher-rails',  :branch => "rails_5_2"
+# ActionView::Template::Error (No connection pool with 'primary' found.):
+# gem 'ketcherails', git: 'https://github.com/ComPlat/ketcher-rails',  :branch => "rails_5_2"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/smiles.js
+++ app/assets/javascripts/smiles.js
@@ -5,63 +5,93 @@
 "use strict";
 
 let row = '<tr>' +
-'   <td><input type="text" placeholder="smile" name="smileInput" value=""/></td>' +
-'   <td><button class="tableButton" id="addRow">+</button></td>' +
-'   <td><button class="tableButton" id="rmvRow">-</button></td>' +
-'</tr>';
+    '   <td><input type="text" placeholder="smile" name="smileInput" value=""/></td>' +
+    '   <td><button class="tableButton" id="addRow">+</button></td>' +
+    '   <td><button class="tableButton" id="rmvRow">-</button></td>' +
+    '</tr>';
 
-$(function(){
+$(function () {
     // initialize table with one empty row
-$("tbody").append(row);
-    // alert('loaded');
-// make table rows movable TODO
-// $('tbody').sortable();
-$(document).on('click', '#addRow', function() {
-    // give next row length of current row
-    let new_row = row
-    $(this).closest('tr').after(new_row);
-});
+    $("#dyTab tbody").append(row);
+    // make table rows movable TODO
+    // $('tbody').sortable();
+    $(document).on('click', '#addRow', function () {
+        // give next row length of current row
+        let new_row = row
+        $(this).closest('tr').after(new_row);
+    });
 
-$(document).on('click', '#rmvRow', function() {
-    // do not delete last row in table
-    if ($(this).parents('tbody').children().length > 1) {
-        $(this).parents('tr').remove(); // this => - button
+    $(document).on('click', '#rmvRow', function () {
+        // do not delete last row in table
+        if ($(this).parents('tbody').children().length > 1) {
+            $(this).parents('tr').remove(); // this => - button
+        }
+    });
+
+    // test data
+    // let smiles = {
+    //     "smis": [
+    //         "CS(=O)(=O)OCCCBr", 
+    //         "CS(=O)(OCCCBr)=O", 
+    //         "O=C(O)c1ccc(NC(=O)C2CCC2)cc1",
+    //         "O=C(O)c1ccc(cc1)(NC(=O)C2CCC2)",
+    //         "CN(Cc1ccc(C(C)(C)C)cc1)c1cccc2ccccc12.Cl",
+    //         "Cl.CN(Cc1ccc(C(C)(C)C)cc1)c1cccc2ccccc12"
+    //     ]
+    // }
+
+    const smilesUser = {
+        "smis": []
     }
-});
 
-let smiles = {
-    "smis": [
-        "CS(=O)(=O)OCCCBr", 
-        "CS(=O)(OCCCBr)=O", 
-        "O=C(O)c1ccc(NC(=O)C2CCC2)cc1",
-        "O=C(O)c1ccc(cc1)(NC(=O)C2CCC2)",
-        "CN(Cc1ccc(C(C)(C)C)cc1)c1cccc2ccccc12.Cl",
-        "Cl.CN(Cc1ccc(C(C)(C)C)cc1)c1cccc2ccccc12"
-    ]
-}
+    //TODO if smilesUser not empty
+    $('#genIupac').click(function () {
+        // get smiles from user input
+        smilesUser["smis"] = [];
+        $('#dyTab [name="smileInput"]').each(function (i, e) {
+            smilesUser["smis"].push($(e).val());
+        });
 
-const iupacs = () => {
-    fetch('http://0.0.0.0:5000/iupac_name', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(smiles),
-      })
-  .then(response => response.json())
-  .then(smiles => console.log(smiles));
-//   .catch(error) => 
-//     console.error('Error:', error);
-}
+        // fetch data from machine learning model
+        (async () => {
+            const iupacsML = await fetch_iupacs();
+            // clear generates iupacs
+            $("#secShowIupac tbody").empty();
+            // $("#secShowIupac p").append(JSON.stringify(iupacsML));
+            console.log(iupacsML);
+            for (const [key, val] of Object.entries(iupacsML)) {
+                if (key !== "") {
+                    $("#iupacTable tbody").append('<tr><td><b>' + key + '</b><td></tr>')
+                    for (const [key1, val1] of Object.entries(val)) {
+                        $("#iupacTable tbody").append('<tr><td>' + key1 + '<td><td>' + val1 + '<td></tr>')
+                    }
+                }
+            }
+            // $('[name="operation"]').show();
+            // todo copy & save in rails
+            $('#copy').click(function () {
+                navigator.clipboard.writeText(JSON.stringify("iupacsML"));
+            })
+            $('#save').click(function () {
+                let blob_JSON = new Blob([JSON.stringify(iupacsML)], { type: "application/json" });
+                fileDownload(blob_JSON);
+            });
 
-iupacs()
+        })()
+    });
 
-const test = () => {
-    fetch('http://0.0.0.0:5000/test')
-  .then(response => response.json())
-  .then(smiles => console.log(smiles));
-}
+    async function fetch_iupacs() {
+        const response = await fetch('http://0.0.0.0:5000/iupac_name', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(smilesUser),
+        })
 
-test();
+        return await response.json();
+        // TODO catch error
+    }
 
 }); // END 
+
